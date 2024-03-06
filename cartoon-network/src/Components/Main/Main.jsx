6af15904f7e741657967aa320a55a89c57ca1362
Lorrayne <lorraynelimamota@gmail.com>
2024-03-06 12:23:52 -0300

    arquivos criados, inicio main

---
+++ cartoon-network/src/Components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as S from './MainStyle.jsx'
+
+export default function Main(){
+    return(
+        <main>
+            <section>
+                
+            </section>
+            <section>
+
+            </section>
+        </main>
+    )
+}
